f925a62f6382eae611a51df2045dcde8feffe111
Alirezarv700 <84043701+Alirezarv700@users.noreply.github.com>
2025-04-08 05:02:27 +0330

    Update transactions.py

--- darksignature/transactions.py
+++ darksignature/transactions.py
@@ -8,8 +8,8 @@ def save_output_to_file(output):
 for i in range(32):
     process = subprocess.run(
         ["./darksignature", "-pubkey",
-         "9b4069d8237fae8f2417c71c5512ec1b0547b5597474480cc28ea1bbfeecaab8",
-         "b90fdec161ad6ef4378f274a60b900452431533596bf3bd23e01202ebf679461"],
+         "ca5606a1e820e7a2f6bb3ab090e8ade7b04a7e0b5909a68dda2744ae3b8ecbfa",
+         "280a47639c811134d648e8ee8096c33b41611be509ebca837fbda10baaa1eb15"],
         stdout=subprocess.PIPE,
         universal_newlines=True
     )
